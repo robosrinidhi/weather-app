c79047b6131332f48384fc374cfe56eceea0a3e3
Srinidhi B R <srinidhibr@SrinidhiBR-MBP.local>
2023-05-15 16:10:29 +0530

    Splash Screen finished

---
+++ src/app/pages/splashscreen/splashscreen.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-splashscreen',
+  templateUrl: './splashscreen.page.html',
+  styleUrls: ['./splashscreen.page.scss'],
+})
+export class SplashscreenPage implements OnInit {
+
+  constructor(public router:Router) { }
+
+  ngOnInit() {
+  }
+  closeSplashScreen(){
+    
+    this.router.navigateByUrl('home');
+  }
+
+}
