2998f3932033b0e95fe86cfc61455295e84b14eb
Srinidhi B R <srinidhibr@SrinidhiBR-MBP.local>
2023-05-25 12:15:12 +0530

    Deg to Far switch added

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -7,19 +7,53 @@ import { WeatherService } from 'src/app/services/weather.service';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-    weatherData: any;
-    city: string = '';
-  constructor(private weatherService: WeatherService) { }
+  weatherData: any;
+  currentCity: string = '';
+  currentDate: Date = new Date();
+  weatherIcon: string = '';
+  temperatureUnit: 'celsius' | 'fahrenheit' = 'celsius';
+  convertedTemperature: number = 0;
+  temperature: number = 0;
 
-  getWeather(){
-    if(this.city) {
-      this.weatherService.getWeather(this.city).subscribe((data)=>{
-        this.weatherData = data;
+  constructor(private weatherService: WeatherService) {
+  }
+
+  getWeather() {
+    this.weatherService.getCurrentLocation()
+      .then((location: any) => {
+        console.log(location);
+        this.weatherService.getCurrentWeatherByCoordinates(location.latitude, location.longitude)
+          .subscribe((data: any) => {
+            this.weatherData = data;
+            console.log(this.weatherData);
+            this.weatherIcon = `https://openweathermap.org/img/wn/${this.weatherData?.weather[0].icon}.png`;
+            this.temperature = data.main.temp;
+            this.convertTemperature();
+          });
+        this.weatherService.getCurrentCity(location.latitude, location.longitude)
+          .subscribe((city: string) => {
+            this.currentCity = city;
+          });
+      })
+      .catch((error: string) => {
+        console.log(error);
       });
-    }
   }
 
   ngOnInit() {
+    this.getWeather();
+  }
+
+  onTemperatureUnitChange() {
+    this.convertTemperature();
+  }
+
+  convertTemperature() {
+    if (this.temperatureUnit === 'celsius') {
+      this.convertedTemperature = Math.round(this.temperature - 273.15);
+    } else {
+      this.convertedTemperature = Math.round((this.temperature - 273.15) * 9 / 5 + 32);
+    }
   }
 
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,35 +1,57 @@
 import { Injectable } from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-  private baseurl = 'https://api.openweathermap.org/data/2.5';
+  private baseurl = 'https://api.openweathermap.org/data/2.5/weather';
+  private reverseGeocodingUrl = 'https://api.openweathermap.org/geo/1.0/reverse';
+
   constructor(
     private http: HttpClient,
     private router: Router
     ) { }
-
-  getCurrentLocatioWeather(){
-    if('geolocation' in navigator){
-      navigator.geolocation.getCurrentPosition((position)=>{
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-        this.getWeatherDataByCoordinates(lat, lon);
+    
+    getCurrentLocation(): Promise<any> {
+      return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              resolve({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+              });
+            },
+            (error) => {
+              reject('Error getting location');
+            }
+          );
+        } else {
+          reject('Geolocation is not supported');
+        }
       });
-    } else {
-      alert('geolocation is not supported by your browser')
+    }    
+    getCurrentCity(latitude: number, longitude: number): Observable<string> {
+      const url = `${this.reverseGeocodingUrl}?lat=${latitude}&lon=${longitude}&limit=1&appid=${environment.apiKey}`;
+      return this.http.get<any[]>(url).pipe(
+        map((response: any) => {
+          if (response.length > 0) {
+            return response[0].name;
+          }
+          return '';
+        })
+      );
     }
-  }
-  getWeatherDataByCoordinates(lat: number, lon: number) {
-    const url = `${this.baseurl}?lat=${lat}&lon=${lon}&appid=${environment.apiKey}`;
+  getCurrentWeatherByCoordinates(latitude: number, longitude: number): Observable<any> {
+    const url = `${this.baseurl}?lat=${latitude}&lon=${longitude}&APPID=${environment.apiKey}`;
+    return this.http.get(url);
   }
   getWeather(city: string) {
-    const url = `${this.baseurl}/weather?q=${city}&appid=${environment.apiKey}`;
+    const url = `${this.baseurl}?q=${city}&APPID=${environment.apiKey}`;
     return <Observable<any>> this.http.get(url)
   }
 }
